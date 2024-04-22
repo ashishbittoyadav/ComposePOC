3e66c37825d781788fdbed26dc6db344852498c9
ashishbittoyadav <ashishbitto07@gmail.com>
2024-04-22 13:00:56 +0530

    object creation of the class dependent on the Interface (as parameter) demonstrated in the MainActivity class.

--- app/src/main/java/com/example/melz/MainActivity.kt
+++ app/src/main/java/com/example/melz/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.melz
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -17,17 +18,41 @@ import com.example.melz.model.Category
 import com.example.melz.ui.theme.MelzTheme
 import com.example.view_model.MealViewModel
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
+import javax.inject.Singleton
 
+interface HiltInterface{
+    fun doSomething(result:String)
+}
+@Module
+@InstallIn(ActivityComponent::class)
+class MyModule{
+    @Provides
+    fun provideHiltInterface(@ActivityContext context: Context):HiltInterface{
+        return context as HiltInterface
+    }
+}
+
+class Test @Inject constructor(private val hiltInterface: HiltInterface){
+    fun callTest(){
+        hiltInterface.doSomething("do something.")
+    }
+}
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(),HiltInterface {
+
+    @Inject lateinit var test: Test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MelzTheme {
+                test.callTest()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,5 +62,9 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun doSomething(result: String) {
+        Log.d("HiltInterface.TAG", "doSomething: name $result")
     }
 }
