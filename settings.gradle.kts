08f9e5f49d2638d916fe0c09492f52fcfc962c4c
ashishbittoyadav <ashishbitto07@gmail.com>
2024-04-25 12:15:51 +0530

    add artifact

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,6 @@
+import java.io.FileInputStream
 import java.net.URI
+import java.util.Properties
 
 include(":networking")
 
@@ -17,12 +19,23 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    val properties = Properties()
+    properties.load(FileInputStream(File("local.properties")))
+
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
         maven("https://jitpack.io")
         maven { url = URI("https://kotlin.bintray.com/kotlinx")  }
+
+        maven {
+            url = uri("https://maven.pkg.github.com/ashishbittoyadav/ComposePOC")
+            credentials {
+                username = properties.getProperty("username", "")
+                password = properties.getProperty("token", "")
+            }
+        }
     }
 }
 
