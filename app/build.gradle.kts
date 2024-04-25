c2084ecdceceab2d9a92c77d29ad6f552b29e3e7
ashishbittoyadav <ashishbitto07@gmail.com>
2024-04-25 14:23:24 +0530

    use stable version of networking.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(project(":networking"))
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -74,39 +74,23 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
     implementation(libs.coil)
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.okhttp)
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
 //    implementation(libs.ktor.client.serialization)
     implementation(libs.kotlinx.serialization.json)
-    implementation(libs.ktor.client.cio)
 
-    implementation(libs.ktor.client.android)
-    implementation(libs.ktor.client.serialization)
-    implementation(libs.ktor.client.json)
-    implementation(libs.ktor.client.logging)
-
-    implementation(libs.ktor.client.android)
-    implementation(libs.ktor.client.json)
-    implementation(libs.ktor.client.serialization)
-    implementation(libs.ktor.client.logging)
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.ktor.client.logging)
-    implementation(libs.ktor.client.cio)
     implementation(libs.bundles.ktor)
-    implementation(libs.ktor.client.content.negotiation)
     implementation(libs.kotlin.serialization)
     implementation(libs.media.kamel)
     implementation(libs.koin.compose)
-    implementation(libs.ktor.server.status.pages)
     implementation(libs.coil.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     implementation(libs.androidx.navigation.compose)
 
+    //room database
     implementation(libs.room.ktx)
     implementation(libs.room.runtime)
     ksp(libs.room.compiler)
@@ -119,8 +103,11 @@ dependencies {
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
-    project(":networking")
-//    implementation(libs.networking)
+//  use local module
+//  project(":networking")
+
+//  use production library
+    implementation(libs.networking)
 }
 
 kapt {
