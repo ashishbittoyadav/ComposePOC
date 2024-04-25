9ce45796202294b58ab1ad26c5ed2f52e898b586
ashishbittoyadav <ashishbitto07@gmail.com>
2024-04-25 13:35:52 +0530

    commented local properties logic from the settings.gradle file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(project(":networking"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -118,6 +119,7 @@ dependencies {
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
+    project(":networking")
 //    implementation(libs.networking)
 }
 
--- app/src/main/java/com/example/melz/network/KtorHelper.kt
+++ app/src/main/java/com/example/melz/network/KtorHelper.kt
@@ -1,95 +1,95 @@
-package com.example.melz.network
-
-import android.util.Log
-import com.example.melz.model.Category
-import com.example.melz.model.MealCategory
-import io.ktor.client.HttpClient
-import io.ktor.client.call.HttpClientCall
-import io.ktor.client.call.body
-import io.ktor.client.call.receive
-import io.ktor.client.engine.android.Android
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.features.json.JsonSerializer
-import io.ktor.client.plugins.ClientRequestException
-import io.ktor.client.plugins.DefaultRequest
-import io.ktor.client.plugins.HttpResponseValidator
-import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.ResponseException
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logging
-import io.ktor.client.plugins.observer.ResponseObserver
-import io.ktor.client.request.accept
-import io.ktor.client.request.header
-import io.ktor.client.statement.HttpResponse
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpStatusCode
-import io.ktor.http.contentType
-import io.ktor.serialization.kotlinx.json.json
-import io.ktor.server.plugins.statuspages.*
-import kotlinx.serialization.json.Json
-import kotlin.Exception
-
-private const val NETWORK_TIME_OUT = 6_000L
-val httpClientAndroid = HttpClient(Android) {
-    install(ContentNegotiation) {
-        json(
-            Json {
-                prettyPrint = true
-                isLenient = true
-                useAlternativeNames = true
-                ignoreUnknownKeys = true
-                encodeDefaults = false
-            }
-        )
-    }
-
-    install(HttpTimeout) {
-        requestTimeoutMillis = NETWORK_TIME_OUT
-        connectTimeoutMillis = NETWORK_TIME_OUT
-        socketTimeoutMillis = NETWORK_TIME_OUT
-    }
-
-    install(Logging) {
-        logger = object : io.ktor.client.plugins.logging.Logger {
-            override fun log(message: String) {
-                Log.v("Logger Ktor =>", message)
-            }
-        }
-        level = LogLevel.ALL
-    }
-
-//    install(ResponseObserver) {
-//        onResponse { response ->
-////                if(response.status.value in 200..299){
-////                    apiCallback.onResponse(response.call,response)
-////                }else{
-////                    apiCallback.onFailure(response.call, Exception(response.status.value.toString()+":"+response.status.description))
-////                }
-//            Log.d("HTTP status:", "${response.status.value}")
+//package com.example.melz.network
+//
+//import android.util.Log
+//import com.example.melz.model.Category
+//import com.example.melz.model.MealCategory
+//import io.ktor.client.HttpClient
+//import io.ktor.client.call.HttpClientCall
+//import io.ktor.client.call.body
+//import io.ktor.client.call.receive
+//import io.ktor.client.engine.android.Android
+//import io.ktor.client.engine.cio.CIO
+//import io.ktor.client.features.json.JsonSerializer
+//import io.ktor.client.plugins.ClientRequestException
+//import io.ktor.client.plugins.DefaultRequest
+//import io.ktor.client.plugins.HttpResponseValidator
+//import io.ktor.client.plugins.HttpTimeout
+//import io.ktor.client.plugins.ResponseException
+//import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+//import io.ktor.client.plugins.defaultRequest
+//import io.ktor.client.plugins.logging.LogLevel
+//import io.ktor.client.plugins.logging.Logging
+//import io.ktor.client.plugins.observer.ResponseObserver
+//import io.ktor.client.request.accept
+//import io.ktor.client.request.header
+//import io.ktor.client.statement.HttpResponse
+//import io.ktor.http.ContentType
+//import io.ktor.http.HttpHeaders
+//import io.ktor.http.HttpStatusCode
+//import io.ktor.http.contentType
+//import io.ktor.serialization.kotlinx.json.json
+//import io.ktor.server.plugins.statuspages.*
+//import kotlinx.serialization.json.Json
+//import kotlin.Exception
+//
+//private const val NETWORK_TIME_OUT = 6_000L
+//val httpClientAndroid = HttpClient(Android) {
+//    install(ContentNegotiation) {
+//        json(
+//            Json {
+//                prettyPrint = true
+//                isLenient = true
+//                useAlternativeNames = true
+//                ignoreUnknownKeys = true
+//                encodeDefaults = false
+//            }
+//        )
+//    }
+//
+//    install(HttpTimeout) {
+//        requestTimeoutMillis = NETWORK_TIME_OUT
+//        connectTimeoutMillis = NETWORK_TIME_OUT
+//        socketTimeoutMillis = NETWORK_TIME_OUT
+//    }
+//
+//    install(Logging) {
+//        logger = object : io.ktor.client.plugins.logging.Logger {
+//            override fun log(message: String) {
+//                Log.v("Logger Ktor =>", message)
+//            }
+//        }
+//        level = LogLevel.ALL
+//    }
+//
+////    install(ResponseObserver) {
+////        onResponse { response ->
+//////                if(response.status.value in 200..299){
+//////                    apiCallback.onResponse(response.call,response)
+//////                }else{
+//////                    apiCallback.onFailure(response.call, Exception(response.status.value.toString()+":"+response.status.description))
+//////                }
+////            Log.d("HTTP status:", "${response.status.value}")
+////        }
+////    }
+//
+//
+//    install(DefaultRequest) {
+//        header(HttpHeaders.ContentType, ContentType.Application.Json)
+//    }
+//
+//    defaultRequest {
+//        contentType(ContentType.Application.Json)
+//        accept(ContentType.Application.Json)
+//    }
+//
+//    HttpResponseValidator {
+//        validateResponse { response ->
+//            val statusCode = response.status.value
+//            if (statusCode !in 200..299) {
+//                throw ClientRequestException(response, response.status.description)
+//            }
 //        }
 //    }
-
-
-    install(DefaultRequest) {
-        header(HttpHeaders.ContentType, ContentType.Application.Json)
-    }
-
-    defaultRequest {
-        contentType(ContentType.Application.Json)
-        accept(ContentType.Application.Json)
-    }
-
-    HttpResponseValidator {
-        validateResponse { response ->
-            val statusCode = response.status.value
-            if (statusCode !in 200..299) {
-                throw ClientRequestException(response, response.status.description)
-            }
-        }
-    }
-
-
-}
+//
+//
+//}
